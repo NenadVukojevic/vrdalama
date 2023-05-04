9762868c719c1afa8b99ecc0e525286ece0aad62
NenadVUkojevic <nenad.vukojevic@gmail.com>
2023-05-04 16:59:59 +0200

    maze parsing

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ pygame.init()
 HEIGHT = 800
 WIDTH = 600
 
+TILE_HEIGHT = 18
+TILE_WIDTH = 35
+
+
 brick_tile = pygame.image.load('img/brick.png')  
 
+maze = ['00F0BAC000AAAABCDA0000A0000000',
+        '00F000BAC000AAAABCDA00A0000000',
+        '00F000000BAC000AAAA000A0000000',
+        '0000000F0BAC0ABCDA0000A0000000',
+        '00F0000BAC000AAAABC000A0000000']
 
 screen = pygame.display.set_mode((HEIGHT,WIDTH))  
 done = False  
   
 def redraw():
-    screen.blit(brick_tile, (20, 40))
-    screen.blit(brick_tile, (80, 80))
-    screen.blit(brick_tile, (120, 80))
-    screen.blit(brick_tile, (160, 80))
-    screen.blit(brick_tile, (200, 80))
+    for j in range(len(maze)):
+        line = maze[j]
+        for i in range(len(line)):
+            if line[i:i+1] != '0':
+                screen.blit(brick_tile, ( TILE_WIDTH* i, TILE_HEIGHT * j))
     pygame.display.flip()  
 
 while not done:  
