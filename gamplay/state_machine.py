988f2a563a8234ea193d6bd66859edff8f4c067b
NenadVUkojevic <nenad.vukojevic@gmail.com>
2023-05-08 17:00:02 +0200

    basic state machine

---
+++ gamplay/state_machine.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+class States(Enum):
+    INIT = 0
+    PLAY_LEVEL = 1
+    RESULT_LEVEL = 2
+    LOST_LIVES = 3
+    END_GAME = 4
+    WON = 5
+
+class Actions(Enum):
+    START = 0
+    EXIT = 1
+    FINISH_LEVEL = 2
+    LOST_LIFE = 3
+    
+
+class StateMachine:
+    def __init__(self):
+        self.state = States.INIT
+
+    def transit(self, action):
+        if self.state == States.INIT:
+            if action == Actions.START:
+                self.state = States.PLAY_LEVEL
+        elif self.state == States.PLAY_LEVEL:
+            if action == Actions.FINISH_LEVEL:
+                self.state = States.RESULT_LEVEL
+            if action == Actions.LOST_LIFE:
+                self.state = States.LOST_LIVES
+        elif self.state == States.RESULT_LEVEL:
+            if action == Actions.START:
+                self.state = States.PLAY_LEVEL
+            if action == Actions.EXIT:
+                self.state = States.END_GAME
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame  
-  
+
+from gamplay.state_machine import StateMachine, Actions
+
 pygame.init()  
+
+stateMachine = StateMachine()
+
 
 HEIGHT = 800
 WIDTH = 600
@@ -14,24 +19,18 @@ brick_red = pygame.image.load('img/brick_red.png')
 brick_green = pygame.image.load('img/brick_green.png') 
 brick_blue = pygame.image.load('img/brick_blue.png') 
 maze = [
-        '002000000000000000000000000000',
-        '001111111111111111111111111100',
-        '0040000BAC000AAAABC00000000000',
-        '001111111111111111111111111100',
-        '002000000000000000000000000000',
-        '001111011111111111111111111100',
-        '0040000BAC000AAAABC00000000000',
-        '001111111111111111101111111100',
-        '002000000000000000000000000000',
-        '001111111111111111111111111100',
-        '0040000BAC000AAAABC00000000000',
-        '001111111111111111111111011100',
-        '0040000BAC000AAAABC00000000000',
-        '00110111111111111111111A111100',
-        '002000000000000000000000000000',
-        '001111111111110111111110111100',
-        '004000000000000000000000001000',
-        '001111111111111111111111111100']
+        '00000000000000000000',
+        '000000AAAAAAAAA00000',
+        '00000ACCCCCCCCCA0000',
+        '00000ACCCCCCCCCA0000',
+        '00000ACCCCCCCCCA0000',
+        '00000ACCCCCCCCCA0000',
+        '000000AAAAAAAAA00000',
+        '00000000000000000000',
+        '0000EEEEEEEEEEEEE000',
+        '00000000000000000000',
+
+        ]
 
 screen = pygame.display.set_mode((HEIGHT,WIDTH))  
 done = False  
@@ -41,13 +40,13 @@ def redraw():
         line = maze[j]
         for i in range(len(line)):
             if line[i:i+1] in '123456789':
-                screen.blit(brick_tile, ( TILE_WIDTH* i, TILE_HEIGHT * j))
+                screen.blit(brick_tile, ( TILE_WIDTH/2 + TILE_WIDTH* i, TILE_HEIGHT * j))
             elif line[i:i+1] in 'AB':
-                screen.blit(brick_red, ( TILE_WIDTH* i, TILE_HEIGHT * j))
+                screen.blit(brick_red, ( TILE_WIDTH/2 + TILE_WIDTH* i, TILE_HEIGHT * j))
             elif line[i:i+1] in 'CD':
-                screen.blit(brick_blue, ( TILE_WIDTH* i, TILE_HEIGHT * j))
+                screen.blit(brick_blue, (TILE_WIDTH/2 +  TILE_WIDTH* i, TILE_HEIGHT * j))
             elif line[i:i+1] in 'EF':
-                screen.blit(brick_green, ( TILE_WIDTH* i, TILE_HEIGHT * j))
+                screen.blit(brick_green, ( TILE_WIDTH/2 +TILE_WIDTH* i, TILE_HEIGHT * j))
     pygame.display.flip()  
 
 while not done:  
